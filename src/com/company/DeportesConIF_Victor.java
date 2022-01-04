96402a8ca25ced848d55dfcb1a2c3c7b5602a28c
Victor <correoparapruebasinformaticas@gmail.com>
2022-01-04 12:59:07 +0100

    SE HAN AÑADIDO LAS EXCEPCIONES

--- src/com/company/DeportesConIF_Victor.java
+++ src/com/company/DeportesConIF_Victor.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DeportesConIF_Victor {
@@ -6,24 +7,33 @@ public class DeportesConIF_Victor {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Introduce la temperatura");
-        int temperatura = sc.nextInt();
+        try{
+            System.out.println("Introduce la temperatura: ");
+            int temperatura = sc.nextInt();
 
-        if (temperatura > 30){
-            System.out.println("tu deporte es natación");
+            if (temperatura > 30){
+                System.out.println("tu deporte es natación");
+            }
+            else if (temperatura > 15 && temperatura <=30){
+                System.out.println("tu deporte es el Golf");
+            }
+            else if (temperatura >5 && temperatura <= 15){
+                System.out.println("tu deporte el el Tenis");
+            }
+            else if (temperatura > -10 && temperatura <= 5){
+                System.out.println("tu deporte es el esquí");
+            }
+            else if (temperatura <= -10){
+                System.out.println("tu deporte son las damas");
+            }
+        }catch (InputMismatchException errorDatos){
+            System.out.println("no puedes introducir un valor no numerico: " + errorDatos);
         }
-        else if (temperatura > 15 && temperatura <=30){
-            System.out.println("tu deporte es el Golf");
-        }
-        else if (temperatura >5 && temperatura <= 15){
-            System.out.println("tu deporte el el Tenis");
-        }
-        else if (temperatura > -10 && temperatura <= 5){
-            System.out.println("tu deporte es el esquí");
-        }
-        else if (temperatura <= -10){
-            System.out.println("tu deporte son las damas");
-        }
+
+
+
+
+
 
         //modificado
     } //final del main
---
+++ src/com/company/DeportesConSwitchCase_Victor.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class DeportesConSwitchCase_Victor {
+
+    public static void main (String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            System.out.println("introduce las opciones:");
+            String temperaturaString = sc.nextLine();
+            int temperaturaInt = Integer.parseInt(temperaturaString);
+
+            switch (temperaturaInt){
+
+                case 1:
+                    System.out.println("estas dentro del caso 1");
+                    break;
+                case 2:
+                    System.out.println("estas dentro del caso 2");
+                    break;
+                case 3:
+                    System.out.println("estas dentro del caso 1");
+                    break;
+                case 4:
+                    System.out.println("estas dentro del caso 1");
+                    break;
+
+            }//final del switch
+        }catch (NumberFormatException error){
+            System.out.println("se ha producido un error con el tipo de dato, error: " + error);
+        }
+
+
+
+
+
+    }//final del main
+}//final de la clase
